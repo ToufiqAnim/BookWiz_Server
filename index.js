8d75fdc15f7cb60b12168f0c58668e36c47b9d8b
ToufiqAnim <toufiq9610@gmail.com>
2024-06-04 05:19:04 +0600

    books curd operation done

--- index.js
+++ index.js
@@ -14,6 +14,50 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
     },
   });
+  async function run() {
+    try {
+        await client.connect();
+        const booksDB=client.db('booksDB');
+        const userDB=client.db('userDB');
+        const booksCollection = booksDB.collection("booksCollection");
+        const userCollection = userDB.collection("userCollection");
+
+        app.post("/books", async (req, res) => {
+            const booksData = req.body;
+            const result = await booksCollection.insertOne(booksData);
+            res.send(result);
+          });
+          app.get("/books", async (req, res) => {
+            const booksData = booksCollection.find();
+            const result = await booksData.toArray();
+            res.send(result);
+          });
+          app.get("/books/:id", async (req, res) => {
+            const id = req.params.id;
+            const booksData = await booksCollection.findOne({
+              _id: new ObjectId(id),
+            });
+            res.send(booksData);
+          });
+          app.patch("/books/:id",  async (req, res) => {
+            const id = req.params.id;
+            const updatedData = req.body;
+            const result = await booksCollection.updateOne(
+              { _id: new ObjectId(id) },
+              { $set: updatedData }
+            );
+            res.send(result);
+          });
+          app.delete("/books/:id", verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const result = await booksCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+          });
+        console.log("Database is connected");
+    } catch (error) {
+        
+    }
+  }
 
 app.get("/", (req, res) => {
     res.send("Route is working");
