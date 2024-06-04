96aa2cce1d02771032c6dae5dd48b6a74cd1af1a
ToufiqAnim <toufiq9610@gmail.com>
2024-06-04 20:04:18 +0600

    vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 
 app.use(cors());
 app.use(express.json());
